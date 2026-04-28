6d5cb6b3bf5a5694434e63b4983eb0ab2b086bb7
R-o-program <ronaldvitors.castro@gmail.com>
2026-04-28 11:17:31 -0300

    conclusão nível novato
    Implemented user input for city attributes and calculated comparison metrics.

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,43 +1,121 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 2 - Comparação das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
-// Siga os comentários para implementar cada parte do desafio.
+int main(){
+    //declaração de variáveis
+    char estado1[20], estado2[20];
+    char codigo1[3], codigo2[3];
+    char cidade1[20], cidade2[20];
+    unsigned long int populacao1, populacao2; //%lu para leitura e exibição de unsigned long int
+    float area1, area2;
+    float pib1, pib2;
+    int qtd_ponto_turistico1, qtd_ponto_turistico2;
+    float dens_populacional1, dens_populacional2;
+    float pib_per_capita1, pib_per_capita2; 
+    float superPoder1, superPoder2;
 
-int main() {
-    // Definição das variáveis para armazenar as propriedades das cidades
-    // Você pode utilizar o código do primeiro desafio
+    //coleta de informações da carta 1
+    printf("***************SUPER TRUNFO***************\n");
+    printf("Digite o nome do estado 1: \n");
+    scanf("%s", estado1);
+    printf("Digite o código 1\n(o código é composto pela primeira letra do nome do estado e dois números. Exemplo: A01):\n");
+    scanf("%s", codigo1);
+    printf("Digite a cidade do estado 1: \n");
+    scanf("%s", cidade1);
+    printf("Digite a população da cidade 1: \n");
+    scanf("%lu", &populacao1);
+    printf("Digite a área em km² da cidade 1: \n");
+    scanf("%f", &area1);
+    printf("Digite o PIB da cidade 1: \n");
+    scanf("%f", &pib1);
+    printf("Digite a quantidade de pontos turísticos da cidade1: \n");
+    scanf("%d", &qtd_ponto_turistico1);
 
+    //coleta de informações da carta 2
+    printf("***************SUPER TRUNFO***************\n");
+    printf("Digite o nome do estado 2: \n");
+    scanf("%s", estado2);
+    printf("Digite o código 2\n(o código é composto pela primeira letra do nome do estado e dois números. Exemplo: A01):\n");
+    scanf("%s", codigo2);
+    printf("Digite a cidade do estado 2: \n");
+    scanf("%s", cidade2);
+    printf("Digite a população da cidade 2: \n");
+    scanf("%lu", &populacao2);
+    printf("Digite a área em km² da cidade 2: \n");
+    scanf("%f", &area2);
+    printf("Digite o PIB da cidade 2: \n");
+    scanf("%f", &pib2);
+    printf("Digite a quantidade de pontos turísticos da cidade 2: \n");
+    scanf("%d", &qtd_ponto_turistico2);
+
+    //cálculo de densidade populacional
+    dens_populacional1 = (float)populacao1 / area1;
+    dens_populacional2 = (float)populacao2 / area2;
     
-    // Cadastro das Cartas:
-    // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
-    // utilizando a função scanf para capturar as entradas.
-    // utilize o código do primeiro desafio
+    //cálculo do PIB per capita
+    pib_per_capita1 = (float)(pib1 * 1000000000) / populacao1; // multiplicamos o PIB por 1 bilhão para converter de bilhões para reais
+    pib_per_capita2 = (float)(pib2 * 1000000000) / populacao2; // (float) é para a conversão explicita
 
-    // Exemplo:
-    // printf("Digite o código da cidade: ");
-    // scanf("%s", codigo);
-    // 
-    // (Repita para cada propriedade)
+    //calculando o superpoder
+    superPoder1 = (populacao1 + area1 + pib1 + qtd_ponto_turistico1 + (1/dens_populacional1));
+    superPoder2 = (populacao2 + area2 + pib2 + qtd_ponto_turistico2 + (1/dens_populacional2));
 
-    // Comparação de Cartas:
-    // Desenvolva a lógica de comparação entre duas cartas.
-    // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
+    //exibindo os resultados
+    printf("\n");
+    printf("Carta 1:\n");
+    printf("Estado: %s\n", estado1);
+    printf("Código: %s\n", codigo1);
+    printf("Cidade: %s\n", cidade1);
+    printf("População: %lu\n", populacao1);
+    printf("Área: %.2f km²\n", area1);
+    printf("PIB: %.2f bilhões de reais\n", pib1);
+    printf("Número de pontos turísticos: %d\n", qtd_ponto_turistico1);
+    printf("Densidade populacional: %.2f hab/km²\n", dens_populacional1);
+    printf("PIB per capita: %.2f reais\n", pib_per_capita1); 
+    printf("Superpoder: %.2f\n", superPoder1);   
+    printf("\n");
+    printf("Carta 2:\n");
+    printf("Estado: %s\n", estado2);
+    printf("Código: %s\n", codigo2);
+    printf("Cidade: %s\n", cidade2);
+    printf("População: %lu\n", populacao2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhões de reais\n", pib2);
+    printf("Número de pontos turísticos: %d\n", qtd_ponto_turistico2);
+    printf("Densidade populacional: %.2f hab/km²\n", dens_populacional2);
+    printf("PIB per capita: %.2f reais\n", pib_per_capita2);
+    printf("Superpoder: %.2f\n", superPoder2);
 
-    // Exemplo:
-    // if (populacaoA > populacaoB) {
-    //     printf("Cidade 1 tem maior população.\n");
-    // } else {
-    //     printf("Cidade 2 tem maior população.\n");
-    // }
+    //comparação de cartas
+    printf("\nComparação de cartas (Atributo: Superpoder):\n");
+    printf("Carta 1 - %s (%s): %.2f\n", cidade1, estado1, superPoder1);
+    printf("Carta 2 - %s (%s): %.2f\n", cidade2, estado2, superPoder2);
+    if(superPoder1 > superPoder2){
+	printf("Resultado: Carta 1 (%s) venceu!\n", cidade1);
+    } else {
+	printf("Resultado: Carta 2 (%s) venceu!\n", cidade2);
+    }
 
-    // Exibição dos Resultados:
-    // Após realizar as comparações, exiba os resultados para o usuário.
-    // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
+    /*Carta 1:
+Nome: Amazonas
+Código: A01
+Cidade: Manaus
+População: 2063689
+Área: 11278893.00 km²
+PIB: 127.60 bilhões de reais
+Número de pontos turísticos: 20
+Densidade populacional: 0.18 hab/km²
+PIB per capita: 61831.02 reais
+Superpoder: 13342735.00
 
-    // Exemplo:
-    // printf("A cidade vencedora é: %s\n", cidadeVencedora);
-
-    return 0;
-}
+Nome: Ceara
+Código: A02
+Cidade: Fortaleza
+População: 2428708
+Área: 312353.00 km²
+PIB: 86.93 bilhões de reais
+Número de pontos turísticos: 15
+Densidade populacional: 7.78 hab/km²
+PIB per capita: 35792.69 reais
+Superpoder: 2741163.25
+*/
+}   
